b3945075c2d5de26be3c417b8e58cf04a14b0b8f
aszyluk <55556005+aszyluk@users.noreply.github.com>
2023-09-07 13:23:14 -0400

    Added Proper Spotify Authorization

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const session = require("express-session");
 
-const static = express.static(__dirname + "/public");
+const staticServe = express.static(__dirname + "/public");
 
 const configRoutes = require("./routes");
 
-app.use("/public", static);
+app.use("/public", staticServe);
+
+app.use(
+  session({
+    name: "SpotifyCookie",
+    secret: process.env.COOKIE_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: { maxAge: 6000000 },
+  })
+);
+
+app.use("/", (req, res, next) => {
+  if (!req.query.code) {
+    res.redirect(
+      `https://accounts.spotify.com/authorize?client_id=${process.env.SPOTIFY_ID}&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000&scope=user-top-read`
+    );
+  } else {
+    next();
+  }
+});
 
 configRoutes(app);
 
--- routes/page.js
+++ routes/page.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const axios = require("axios");
+const queryString = require("node:querystring");
 
 router.get("/", async (req, res) => {
   try {
+    if (!req.session.token) {
+      const { data } = await axios.post(
+        "https://accounts.spotify.com/api/token",
+        queryString.stringify({
+          grant_type: "authorization_code",
+          code: req.query.code,
+          redirect_uri: "http://localhost:3000",
+        }),
+        {
+          headers: {
+            Authorization: `Basic ${process.env.SPOTIFY_AUTH}`,
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+      req.session.token = data.access_token;
+    }
     res.sendFile(path.resolve("static/index.html"));
   } catch (e) {
     // Something went wrong with the server!
